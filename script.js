38f5bea800d05fd22d9b20f12cd0eac5e423b144
MathisSTL <141674101+MathisSTL@users.noreply.github.com>
2024-08-02 23:08:16 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function validateAnswer() {
+    var userInput = document.getElementById("userInput").value;
+    var message = document.getElementById("message");
+    
+    if (userInput.trim().toLowerCase() === "quatre") {
+        message.innerHTML = '<a href="https://mathisstl.github.io/RGB/" target="_blank">https://mathisstl.github.io/RGB/</a>';
+        message.style.color = "green";
+    } else {
+        message.textContent = "Essayez encore";
+        message.style.color = "red";
+    }
+}
